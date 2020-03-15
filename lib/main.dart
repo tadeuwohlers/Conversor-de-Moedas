31ce458f5ac12444713fb35da945459c9ca8dd4a
Tadeu Wohlers <tadeuwohlers@outlook.com>
2020-03-15 19:40:56 -0300

    Estrutura do app

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=cc18dd74";
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Conversor de Moedas"),
+        backgroundColor: Colors.blue,
+        centerTitle: true
+        ),
+    );
+  }
+}
+
